e38119b804db563de871dbc3a9b9fcf7b95e8eed
EllieB-BelliE <ellie_eb@yahoo.com>
2025-05-30 22:55:41 +0100

    streamlit stuff

--- webscrap.py
+++ webscrap.py
@@ -1,13 +1,23 @@
+#to run in cmd streamlit run webscrap.py
+
 import requests
 from bs4 import BeautifulSoup
+import streamlit as st
 
-def main():
+def render():
+    results = fetch_page()
+    st.title("Wiki Find!")
+    st.badge(str(results['status']),icon=":material/network_ping:",color="green")
+    st.markdown(results['url'])
+    st.subheader(results['content'])
+
+def fetch_page():
     webpage = requests.get("https://en.wikipedia.org/wiki/Special:Random")
-    print(webpage)
-    print(webpage.url)
     soup = BeautifulSoup(webpage.content, "html.parser")
     soup_page = soup.find("p")
-    print(soup_page.text)
+    return {"status": webpage.status_code, "url": webpage.url, "content": soup_page.text}
 
-main()
+
+render()
+
 
