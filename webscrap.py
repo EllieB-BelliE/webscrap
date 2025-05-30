f6b3e3f1fd3dfa8410896d7792413faf8e2a103e
EllieB-BelliE <ellie_eb@yahoo.com>
2025-05-30 22:17:02 +0100

    with url

--- webscrap.py
+++ webscrap.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 def main():
     webpage = requests.get("https://en.wikipedia.org/wiki/Special:Random")
     print(webpage)
+    print(webpage.url)
     soup = BeautifulSoup(webpage.content, "html.parser")
     soup_page = soup.find("p")
     print(soup_page.text)
