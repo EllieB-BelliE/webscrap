9f487f7427228613237d17b6d9ea87e15ed3104a
EllieB-BelliE <ellie_eb@yahoo.com>
2025-05-30 22:14:07 +0100

    first commit

---
+++ webscrap.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+def main():
+    webpage = requests.get("https://en.wikipedia.org/wiki/Special:Random")
+    print(webpage)
+    soup = BeautifulSoup(webpage.content, "html.parser")
+    soup_page = soup.find("p")
+    print(soup_page.text)
+
+main()
+
